1cf837b6f5fd4854de65ab5a0d9df03a944b4310
elikond <75093040+elikond@users.noreply.github.com>
2023-01-28 19:43:43 -0800

    Update shap.sh

--- scripts/shap.sh
+++ scripts/shap.sh
@@ -11,4 +11,4 @@ module load python/3.7.4
 module load anaconda/2022.05
 source /gpfs/runtime/opt/anaconda/2022.05/etc/profile.d/conda.sh
 conda activate /gpfs/data/rsingh47/chi3l1_venv
-python ~/shap.py
+python ~/SHAP_GB2.py
